cc0c5269b17a795ea7c0bcfbda7c9b95a8cca64c
Aldo Cambronero Ureña <cambroneroaldo03@gmail.com>
2020-10-15 23:29:07 -0600

    Quitando clases inecesarias

--- Tarea-2/googleTest/persona_unittest.cpp
+++
@@ -1,9 +0,0 @@
-#include "persona.h"
-#include "gtest/gtest.h"
-namespace{
-TEST(persona, getEdad){
-    persona aldo(20);
-    //EXPECT_EQ("Aldo",aldo.getEdad());
-    EXPECT_EQ(20,aldo.getEdad());
-}
-}
